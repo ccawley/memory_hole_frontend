d64deb180cdb08528d487d668296ac348fc1875b
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-05 13:13:44 -0700

    created my first component

---
+++ src/components/Bed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Bed extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World! Did you make your bed today?</p>
+      </div>
+    );
+  }
+}
+
+export default Bed;
