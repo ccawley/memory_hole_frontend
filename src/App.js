08959fe13fcebe216e1cd5fbb6f815cf9052866c
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-05 21:50:12 -0700

    created a bed component that is sucesfully rendering

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+
+  state={
+    bed: false
+  }
+
+  updateState = (value) => {
+    this.setState({ bed: value })
+  }
+
   render() {
+    console.log('app rerender');
     return (
       <div className="Container">
         <h1>MemoryHole</h1>
-        <Bed />
+        <Bed bed={this.state.bed} onSubmit={this.updateState}/>
       </div>
     );
   }
--- src/components/Bed.js
+++ src/components/Bed.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Responsive, Grid, Form } from 'semantic-ui-react';
 
 class Bed extends Component {
+
+  handleChange = (e, {value}) => {
+    this.props.onSubmit(value)
+  }
+
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <p>Hello World! Did you make your bed today?</p>
-      </div>
+      // <Responsive {...Responsive.onlyComputer}>
+        <Grid>
+          <Grid.Row centered>
+            <Grid.Column textAlign='center' width={8} className='dailyCard'>
+              <Form>
+                <Form.Group>
+                  <label>Did you make your bed?</label>
+                  <Form.Radio label='Sure did!' value='t' onChange={this.handleChange} />
+                  <Form.Radio label='Whoops, I forgot.' value='f' onChange={this.props.onSubmit} />
+                </Form.Group>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      // </Responsive>
     );
   }
 }
