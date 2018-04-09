fe746eef20442c6aa76a1ceefd8815546664802c
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-08 17:39:13 -0700

    added drop down menu to navbar and reworked content and links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,56 @@
-import React, { Component } from 'react'
-import { Menu, Responsive, Icon } from 'semantic-ui-react'
+import React from 'react'
+import { Dropdown, Menu, Responsive } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
-class Header extends Component {
+function HeaderMenu() {
 
-  state = {}
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    const { activeItem } = this.state
-
-    return (
-      <Menu inverted>
+  return (
+    <Responsive {...Responsive.onlyMobile}>
+      <Menu color='inverted'>
         <Menu.Item header>MemoryHole</Menu.Item>
-        <Menu.Item>
-          <Icon link name='content' />
-        </Menu.Item>
+
+        <Menu.Menu position='right'>
+          <Dropdown item icon='content'>
+            <Dropdown.Menu>
+              <Link to="/home">
+                <Dropdown.Item name='home'>
+                  Home
+                </Dropdown.Item>
+              </Link>
+              <Link to="/calender">
+                <Dropdown.Item name='calender'>
+                  Calender
+                </Dropdown.Item>
+              </Link>
+              <Link to="/firstVictory">
+                <Dropdown.Item name='startDay'>
+                  Start Your Day
+                </Dropdown.Item>
+              </Link>
+              <Link to="/picOfDay">
+                <Dropdown.Item name='endDay'>
+                  End Your Day
+                </Dropdown.Item>
+              </Link>
+              <Link to="/about">
+                <Dropdown.Item name='about'>
+                  About / F.A.Q.
+                </Dropdown.Item>
+              </Link>
+              <Link to="/agelapse">
+                <Dropdown.Item name='agelapse'>
+                  Agelapse
+                </Dropdown.Item>
+              </Link>
+              <Dropdown.Item name='logout'>
+                Logout
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
       </Menu>
-    )
-  }
+    </Responsive>
+  )
 }
 
-export default Header
+export default HeaderMenu
