4fab58659c76f386348ed7884951cb4d8a1f8500
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-09 15:32:04 -0700

    trying to get values out of text area

--- src/components/MorningGratitudes.js
+++ src/components/MorningGratitudes.js
@@ -4,21 +4,30 @@ import { Link } from 'react-router-dom';
 
 class MorningGratitudes extends Component {
 
-  state = {}
+  state = { gratitude1: '', gratitude2: '', gratitude3: '' }
 
-  handleChange = (e, { value }) => {
-    this.setState({ value })
-  }
+  handleChange = (e, { gratitude1, value }) => this.setState({ [gratitude1]: value })
 
-  handleSubmit = (e) => {
-    console.log('submit?', this.props);
-    this.props.onSubmit ({
-      gratitude1: this.state.value,
-      gratitude2: this.state.value,
-      gratitude3: this.state.value
+  handleSubmit = () => {
+    const { gratitude1, gratitude2, gratitude3 } = this.state
+
+    this.props.onSubmit({
+      gratitude1: this.state.gratitude1,
+      gratitude2: this.state.gratitude2,
+      gratitude3: this.state.gratitude3,
     })
-    // this.props.history.go('/quote')
+    // this.setState({ submittedName: name, submittedEmail: email })
   }
+
+  // handleSubmit = (e) => {
+  //   console.log('submit?', this.props);
+  //   this.props.onSubmit ({
+  //     gratitude1: this.state.value,
+  //     gratitude2: this.state.value,
+  //     gratitude3: this.state.value
+  //   })
+  //   // this.props.history.go('/quote')
+  // }
 
   render() {
     return (
@@ -30,19 +39,25 @@ class MorningGratitudes extends Component {
                 <Form.Group grouped>
                   <h1>Now, think thankfully!</h1>
                   <h2>Take a moment to think about some things that you are grateful for. Briefly tell me about them below.</h2>
-                  <TextArea
+                  <Form.Input
                     placeholder='What are you thankful for today?'
                     rows={1}
+                    name='gratitude1'
+                    value={gratitude1}
                     onChange={this.handleChange}
                   />
-                  <TextArea
+                  <Form.Input
                     placeholder='What ELSE are you thankful for today?'
                     rows={1}
+                    name='gratitude2'
+                    value={gratitude2}
                     onChange={this.handleChange}
                   />
-                  <TextArea
+                  <Form.Input
                     placeholder='One more gratitude, you got this!'
                     rows={1}
+                    name='gratitude3'
+                    value={gratitude3}
                     onChange={this.handleChange}
                   />
                   <h4>Remember, being happy and grateful is a choice you make daily. Accept that you while you can't control everything, you CAN choose to give yourself the gift of being the happiest version of yourself that you can be. Thinking gratefully and positively will go a long way in acheiving this.</h4>
