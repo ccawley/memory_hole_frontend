db196f6b084fb69650fb978103613eb749ba4248
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-10 10:02:01 -0700

    minor edits

--- src/components/MorningGratitudes.js
+++ src/components/MorningGratitudes.js
@@ -16,18 +16,7 @@ class MorningGratitudes extends Component {
       gratitude2,
       gratitude3
     })
-    // this.setState({ submittedName: name, submittedEmail: email })
   }
-
-  // handleSubmit = (e) => {
-  //   console.log('submit?', this.props);
-  //   this.props.onSubmit ({
-  //     gratitude1: this.state.value,
-  //     gratitude2: this.state.value,
-  //     gratitude3: this.state.value
-  //   })
-  //   // this.props.history.go('/quote')
-  // }
 
   render() {
     return (
@@ -41,27 +30,24 @@ class MorningGratitudes extends Component {
                   <h2>Take a moment to think about some things that you are grateful for. Briefly tell me about them below.</h2>
                   <Form.Input
                     placeholder='What are you thankful for today?'
-                    rows={1}
                     name='gratitude1'
                     value={this.state.gratitude1}
                     onChange={this.handleChange}
                   />
                   <Form.Input
                     placeholder='What ELSE are you thankful for today?'
-                    rows={1}
                     name='gratitude2'
                     value={this.state.gratitude2}
                     onChange={this.handleChange}
                   />
                   <Form.Input
                     placeholder='One more gratitude, you got this!'
-                    rows={1}
                     name='gratitude3'
                     value={this.state.gratitude3}
                     onChange={this.handleChange}
                   />
                   <h4>Remember, being happy and grateful is a choice you make daily. Accept that you while you can't control everything, you CAN choose to give yourself the gift of being the happiest version of yourself that you can be. Thinking gratefully and positively will go a long way in acheiving this.</h4>
-                  <Link to="/dailyGoal"><Button onClick={this.handleSubmit} type='submit'>Next!</Button></Link>
+                  <Link to="/dailyGoal"><Button type='submit' onClick={this.handleSubmit}>Next!</Button></Link>
                 </Form.Group>
               </Form>
             </Grid.Column>
