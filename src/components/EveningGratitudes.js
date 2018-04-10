c288ede63336c9047347e5811af183fb526b2586
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-10 14:17:45 -0700

    evening gratitudes are now being updated in state upon user entry

--- src/components/EveningGratitudes.js
+++ src/components/EveningGratitudes.js
@@ -4,8 +4,18 @@ import { Link } from 'react-router-dom';
 
 class EveningGratitudes extends Component {
 
-  handleSubmit = (e, {value}) => {
-    this.props.onSubmit(value)
+  state = { gratitude4: '', gratitude5: '', gratitude6: '' }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    const { gratitude4, gratitude5, gratitude6 } = this.state
+
+    this.props.onSubmit({
+      gratitude4,
+      gratitude5,
+      gratitude6
+    })
   }
 
   render() {
@@ -18,11 +28,26 @@ class EveningGratitudes extends Component {
                 <Form.Group grouped>
                   <h1>Think thankfully again!</h1>
                   <h2>Take a moment to think about some things that you are grateful for. Briefly tell me about them below.</h2>
-                  <TextArea placeholder='What are you thankful for right now?' rows={1} onSubmit={this.props.onSubmit} />
-                  <TextArea placeholder='What ELSE are you thankful for tonight?' rows={1} onSubmit={this.props.onSubmit} />
-                  <TextArea placeholder='One more gratitude, you got this!' rows={1} onSubmit={this.props.onSubmit} />
+                  <Form.Input
+                    placeholder='What are you thankful for right now?'
+                    name='gratitude4'
+                    value={this.state.gratitude4}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Input
+                    placeholder='What ELSE are you thankful for tonight?'
+                    name='gratitude5'
+                    value={this.state.gratitude5}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Input
+                    placeholder='One more gratitude, you got this!'
+                    name='gratitude6'
+                    value={this.state.gratitude6}
+                    onChange={this.handleChange}
+                  />
                   <h4>Remember, being happy and grateful is a choice you make daily. Accept that you while you can't control everything, you CAN choose to give yourself the gift of being the happiest version of yourself that you can be. Thinking gratefully and positively will go a long way in acheiving this.</h4>
-                  <Link to="/journal"><Button type='submit'>Next!</Button></Link>
+                  <Link to="/journal"><Button type='submit' onClick={this.handleSubmit}>Next!</Button></Link>
                 </Form.Group>
               </Form>
             </Grid.Column>
