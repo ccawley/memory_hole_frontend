20af8bd2b0749ce4f3a544c4b1124f908af71f32
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-10 13:54:23 -0700

    minor change

--- src/components/DailyGoal.js
+++ src/components/DailyGoal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Form, TextArea, Button, Responsive } from 'semantic-ui-react';
+import { Grid, Form, Button, Responsive } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class DailyGoal extends Component {
