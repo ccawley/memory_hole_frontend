8bf20bc76a2063249846162801e2f80c3f1b3c80
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-10 11:17:54 -0700

    refactored so that the resulting val is a boolean

--- src/components/FirstVictory.js
+++ src/components/FirstVictory.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import { Grid, Form, Button, Radio, Responsive } from 'semantic-ui-react';
+import { Grid, Form, Button, Checkbox, Responsive } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class FirstVictory extends Component {
 
-  state = {}
+  state = {didMakeBed: false}
 
-  handleChange = (e, { value }) => this.setState({ value })
+  handleBedChange = (e, { name, value }) => {
+    this.setState({
+      didMakeBed: !this.state.didMakeBed
+    })
+  }
 
   handleSubmit = (e) => {
-    // console.log('submit?', this.props);
-    this.props.onSubmit({ didMakeBed: this.state.value })
-    // this.props.history.go('/quote')
+    const { didMakeBed } = this.state
+
+    this.props.onSubmit({
+      didMakeBed
+    })
   }
+
+  // handleChange = (e, { value }) => this.setState({ value })
+  //
+  // handleSubmit = (e) => {
+  //   // console.log('submit?', this.props);
+  //   this.props.onSubmit({ didMakeBed: this.state.value })
+  //   // this.props.history.go('/quote')
+  // }
 
   render() {
     return (
@@ -34,24 +48,17 @@ class FirstVictory extends Component {
                   </p>
                   <h3>So, did you make your bed?</h3>
                   <Form.Field>
-                    <Radio
-                      label='Sure did!'
+                    <Checkbox
+                      toggle
+                      name='didMakeBed'
+                      label='Aw yeah!'
                       control='input'
                       value='true'
-                      checked={this.state.value === 'true'}
-                      onChange={this.handleChange}
+                      checked={this.state.didMakeBed === true}
+                      onChange={this.handleBedChange}
                     />
                   </Form.Field>
-                  <Form.Field>
-                    <Radio
-                      label='Whoops, I forgot.'
-                      control='input'
-                      value='false'
-                      checked={this.state.value === 'false'}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Field>
-                  <Link to="/beMindful"><Button onClick={this.handleSubmit} type='submit'>Next!</Button></Link>
+                  <Link to="/beMindful"><Button type='submit' onClick={this.handleSubmit}>Next!</Button></Link>
                 </Form.Group>
               </Form>
             </Grid.Column>
