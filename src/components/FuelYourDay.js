e0f96da9ee3b209281e473d78c02659863fb3779
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-11 16:30:38 -0700

    minor edit

--- src/components/FuelYourDay.js
+++ src/components/FuelYourDay.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Grid, Form, Checkbox, Button, Responsive } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
 
 class FuelYourDay extends Component {
 
