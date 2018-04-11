ac1a01b83418aef58bfcd4836fc69288093fca8f
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-11 13:01:21 -0700

    changed structure of render to reflect that you can be logged in or out

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import MorningRoutine from './components/MorningRoutine';
-import EveningRoutine from './components/EveningRoutine';
-import Header from './components/NavBar';
+import Home from './components/Home';
+// import MorningRoutine from './components/MorningRoutine';
+// import EveningRoutine from './components/EveningRoutine';
+// import Header from './components/NavBar';
+import LoginForm from './components/SplashLogin';
 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      is_logged_in: false
+    }
+  }
 
   render() {
     return (
       <BrowserRouter>
         <div className="Container">
-          <Header />
-          <MorningRoutine />
-          <EveningRoutine />
+          {this.state.is_logged_in ? (<Home />) : (<LoginForm />)}
         </div>
+        {/* <div className="Container">
+              {this.state.isLoggedIn ? (
+                <LoginForm />
+              ) : (
+                <Header />
+                <MorningRoutine />
+                <EveningRoutine />
+              )
+            </div> */}
       </BrowserRouter>
     );
   }
