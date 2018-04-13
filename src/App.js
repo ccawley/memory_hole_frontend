65d039775cc996c442ed4a165d0211a08891a1d8
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-12 22:20:41 -0700

    added content to homepage but weather data isn't dynamically rendering yet

--- src/App.js
+++ src/App.js
@@ -19,11 +19,27 @@ const baseURL = 'http://localhost:8000'
 class App extends Component {
   constructor() {
     super();
+    // ---------------------------swap for deployed----------------------------
+    // this.state = {
+    //   is_logged_in: false,
+    //   user_id: null,
+    //   user_name: null,
+    //   first_name: null
+    //   currTemp: 0,
+    //   dayForecast: '',
+    //   weatherIcon: '',
+    //   highTemp: 0
+    // }
+    // ---------------------------swap when developing-------------------------
     this.state = {
-      is_logged_in: false,
-      user_id: null,
-      user_name: null,
-      first_name: null
+      is_logged_in: true,
+      user_id: 1,
+      user_name: 'Busey',
+      first_name: 'Gary',
+      currTemp: 0,
+      dayForecast: null,
+      weatherIcon: '',
+      highTemp: 0
     }
   }
 
@@ -67,11 +83,35 @@ class App extends Component {
       .catch(console.error)
   }
 
+  componentDidMount() {
+    this.getWeather()
+  }
+
+  getWeather = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      let latitude = position.coords.latitude
+      let longitude = position.coords.longitude
+      axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${'6240cc42aaad1d910992a699a33d5d0a'}/${latitude},${longitude}`, {headers: {'x-requested-with': 'curl'}})
+        .then(res => {
+          console.log(res)
+          this.setState({
+            currTemp: res.data.currently.temperature,
+            dayForecast: res.data.hourly.summary,
+            weatherIcon: res.data.hourly.icon,
+            highTemp: res.data.daily.data[0].temperatureMax
+          })
+        })
+        .catch(console.error)
+    },err => {
+      console.log(err)
+    })
+  }
+
   render() {
     return (
       <div className="Container">
         <Route path='/home' render={props => (
-          <Home />
+          <Home firstName={this.state.first_name}/>
         )} />
         <Route path='/login' render={props => (
           <LoginForm onSubmit={this.onLoginSubmit} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import { Grid, Responsive } from 'semantic-ui-react';
+import { Grid, Responsive, Header, Segment } from 'semantic-ui-react';
+import Skycons from 'react-skycons';
+// const SkyIcons = require('react-skycons');
+const DateFormat = require('dateformat');
+const Now = new Date();
 
 class Home extends Component {
+  constructor (props) {
+    super(props)
+  }
+
   render() {
     return (
       <Responsive {...Responsive.onlyMobile}>
         <Grid>
           <Grid.Row centered>
-            <Grid.Column textAlign='center'>
-              <h1>HOME</h1>
+            <Grid.Column textAlign='center' width={14}>
+              <div>
+                <Header as='h2' attached>
+                  Hello {this.props.firstName}
+                </Header>
+                <Header as='h3' attached>
+                  {DateFormat(Now, "dddd, mmmm dS, yyyy")}
+                </Header>
+                <Segment attached>
+                  <h3>{`Currently: ${this.props.currTemp} High: ${this.props.highTemp}`}</h3>
+                  <Skycons
+                    color='black'
+                    icon={this.props.weatherIcon}
+                    autoplay={true}
+                  />
+                  <p>Summary: {this.props.dayForecast}</p>
+                  <p><small>https://darksky.net/poweredby/</small></p>
+                </Segment>
+              </div>
             </Grid.Column>
           </Grid.Row>
         </Grid>
