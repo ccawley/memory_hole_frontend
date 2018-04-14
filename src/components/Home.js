9923e953bf613bbfa83349a2eabc1ac86d6d3b95
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-14 11:16:36 -0700

    added some dummy data and code to be able to play around while not connected to internet

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,73 @@ import Skycons from 'react-skycons';
 const DateFormat = require('dateformat');
 const Now = new Date();
 
+// Swap when connected to internet or deploy ----------------------------------
+// class Home extends Component {
+//
+//   renderWeather = () => {
+//     return (
+//       <div>
+//         <h2>Hello {this.props.firstName}</h2>
+//         <h3>{DateFormat(Now, "dddd, mmmm dS, yyyy")}</h3>
+//         <h3>{`Currently: ${this.props.currTemp} - High: ${this.props.highTemp}`}</h3>
+//         <Skycons
+//           color='black'
+//           icon={this.props.weatherIcon}
+//           autoplay={true}
+//         />
+//         <p>Summary: {this.props.dayForecast}</p>
+//         <p><small>https://darksky.net/poweredby/</small></p>
+//         <div>
+//           <Link to="/morning/firstVictory">
+//             <Button attached='left'>Start Morning</Button>
+//           </Link>
+//           <Link to="/evening/lessons">
+//             <Button attached='right'>Start Evening</Button>
+//           </Link>
+//         </div>
+//       </div>
+//     )
+//   }
+//
+//   render() {
+//     return (
+//       <Responsive {...Responsive.onlyMobile}>
+//         <Grid>
+//           <Grid.Row centered>
+//             <Grid.Column textAlign='center' width={14}>
+//               { this.props.dayForecast ? this.renderWeather() : null}
+//             </Grid.Column>
+//           </Grid.Row>
+//         </Grid>
+//       </Responsive>
+//     );
+//   }
+// }
+
+// Swap when offline ----------------------------------------------------------
 class Home extends Component {
+
+  state = {
+    firstName: 'Curtis',
+    dayForecast: 'Cloudy with a chance of meatballs. Raining spagooter later this evening.',
+    currTemp: 45,
+    highTemp: 58,
+    weatherIcon: 'PARTLY_CLOUDY_DAY',
+
+  }
 
   renderWeather = () => {
     return (
       <div>
-        <h2>Hello {this.props.firstName}</h2>
+        <h2>Hello {this.state.firstName}</h2>
         <h3>{DateFormat(Now, "dddd, mmmm dS, yyyy")}</h3>
-        <h3>{`Currently: ${this.props.currTemp} - High: ${this.props.highTemp}`}</h3>
+        <h3>{`Currently: ${this.state.currTemp} - High: ${this.state.highTemp}`}</h3>
         <Skycons
           color='black'
-          icon={this.props.weatherIcon}
+          icon={this.state.weatherIcon}
           autoplay={true}
         />
-        <p>Summary: {this.props.dayForecast}</p>
+        <p>Summary: {this.state.dayForecast}</p>
         <p><small>https://darksky.net/poweredby/</small></p>
         <div>
           <Link to="/morning/firstVictory">
@@ -38,7 +91,7 @@ class Home extends Component {
         <Grid>
           <Grid.Row centered>
             <Grid.Column textAlign='center' width={14}>
-              { this.props.dayForecast ? this.renderWeather() : null}
+              {this.renderWeather()}
             </Grid.Column>
           </Grid.Row>
         </Grid>
