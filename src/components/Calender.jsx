e7035ed010f1f6d41571fa7d3c3edab2fdeaf0cd
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-16 13:40:55 -0700

    minor changes, cleaning up

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -9,20 +9,11 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 class Calender extends Component {
 
-  // state = { stuff: [] }
-
   constructor (props) {
     super(props)
+    // this.state = { stuff: [] }
     this.state = {
-      // stuff: [],
       startDate: moment(),
-      // id: null,
-      // user_name: null,
-      // first_name: null,
-      // password: null,
-      // created_at: null,
-      // updated_at: null,
-      // user_id: 0,
       did_make_bed: null,
       gratitude_1: null,
       gratitude_2: null,
@@ -77,56 +68,12 @@ class Calender extends Component {
             gratitude_6: data.gratitude_6,
             pic_of_the_day_url: data.pic_of_the_day_url
           }
-          // if (!data.gratitude_1) {
-          //   { did_make_bed: null,
-          //     gratitude_1: null,
-          //     gratitude_2: null,
-          //     gratitude_3: null,
-          //     daily_goal: null,
-          //     goal_microtarget_1: null,
-          //     goal_microtarget_2: null,
-          //     goal_microtarget_3: null,
-          //     had_breakfast: null,
-          //     did_exercise: null,
-          //     inspirational_quote: null,
-          //     quote_author: null,
-          //     journal_entry: null,
-          //     lessons_learned: null,
-          //     wins_brags: null,
-          //     gratitude_4: null,
-          //     gratitude_5: null,
-          //     gratitude_6: null,
-          //     pic_of_the_day_url: data.pic_of_the_day_url
-          //   }
-          // } else {
-          //   { did_make_bed: data.did_make_bed,
-          //     gratitude_1: data.gratitude_1,
-          //     gratitude_2: data.gratitude_2,
-          //     gratitude_3: data.gratitude_3,
-          //     daily_goal: data.daily_goal,
-          //     goal_microtarget_1: data.goal_microtarget_1,
-          //     goal_microtarget_2: data.goal_microtarget_2,
-          //     goal_microtarget_3: data.goal_microtarget_3,
-          //     had_breakfast: data.had_breakfast,
-          //     did_exercise: data.did_exercise,
-          //     inspirational_quote: data.inspirational_quote,
-          //     quote_author: data.quote_author,
-          //     journal_entry: data.journal_entry,
-          //     lessons_learned: data.lessons_learned,
-          //     wins_brags: data.wins_brags,
-          //     gratitude_4: data.gratitude_4,
-          //     gratitude_5: data.gratitude_5,
-          //     gratitude_6: data.gratitude_6,
-          //     pic_of_the_day_url
-          //   }
-          // }
         )
       })
       .catch(err => {
         console.log('error');
         console.log(err)
       })
-    //axios.get('http://localhost:8000/routine', querystring.stringify({date: formatedDate}))
   }
 
   // handleChange = (e, { name, value }) => this.setState({ [name]: value })
